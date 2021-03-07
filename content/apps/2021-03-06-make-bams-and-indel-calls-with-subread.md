a68c8b3e682065916404b0f30f86ba8d71b3eadc
Junli Zhang <zhjl86@gmail.com>
2021-03-06 23:57:05 -0800

    add tool subread

---
+++ content/apps/2021-03-06-make-bams-and-indel-calls-with-subread.md
@@ -0,0 +1,69 @@
+---
+title: Make bams and indel calls with Subread
+author: Junli Zhang
+date: '2021-03-06'
+slug: make-bams-and-indel-calls-with-subread
+categories:
+  - Method
+tags:
+  - bam
+  - indel
+  - subread
+  - NGS
+  - CRISPR
+---
+
+This tool is for **paired end** fastq files (for example, xxx_R1_001.fastq.gz and xxx_R2_001.fastq.gz).  
+Please run the 3 steps below to get the indexed bams from a list of fastq files.  
+**Please make sure you have at least 1.5 Gb of free memory.**
+
+<label for="suffix">Provide the suffix of your fastq files:</label><br>
+Read 1: <input id="suffix1" value="_R1_001.fastq.gz" size="40"><br>
+Read 2: <input id="suffix2" value="_R2_001.fastq.gz" size="40"><br>
+
+<h4>I. Load reference file (a fasta file)</h4>
+<input id="reference" type="file">
+
+<h4>II. Load demultiplexed fastq(.gz) files</h4>
+<input id="fastq" type="file" multiple>
+
+<p id="indexErr" style="color:red;"></p>
+<p id="demoRef" style="display:none;"></p>
+<p id="demoFq" style="display:none;"></p>
+
+<h4>III. Map reads and create bam files</h4>
+
+After loading the template fasta file and all the fastq files, now we will use [subread](http://subread.sourceforge.net/) to create indexed bam files for viewing in the software [IGV](https://software.broadinstitute.org/software/igv/download) and call indels and structure variations.
+
+<button onclick="makeAll()">Map reads and Make bam files</button>
+<p id="bam" style="color:pink;font-style: italic;"></p>
+<button id="download-btn" onclick="downloadAll()" style="visibility:hidden">Download indexed bam files</button>
+<p id="download" style="color:pink;font-style: italic;"></p>
+
+<label for="stderr">Running Summary</label><br>
+<textarea id="stderr" name="stderr" rows="20" cols="100" style="font-family: monospace;font-size: 10px;" placeholder="Software running informaiton will be shown here"></textarea><br>
+
+<script src="/tools/aioli/latest/aioli.js"></script>
+<script src="/libs/subread.js"></script>
+<script src="/libs/FileSaver.min.js"></script>
+<script src="/libs/jszip.min.js"></script>
+
+## Help
+
+This tool is a WebAssembly implementation of [subread](http://subread.sourceforge.net/). It runs commands like this:
+
+```sh
+## index the references
+subread-buildindex -M 1000 -o my_index <your-references.fa>
+## make sorted bam files and vcf files for indels (<=16 bp) 
+#  and structure variaitons (indels > 16 bp and inversions etc)
+subread-align -i my_index -r xxxx_R1_001.fastq.gz -R xxxx_R2_001.fastq.gz
+              -o xxxx.bam -I 16  -sv --sortReadsByCoordinates
+```
+
+Visit the GitHub page for more details: [https://github.com/pinbo/bwa-samtools-web](https://github.com/pinbo/bwa-samtools-web).
+
+## Acknowledgement
+
+- [subread](http://subread.sourceforge.net/) is a great software for calling big indels.
+- Thank [Robert Aboukhalil](https://github.com/robertaboukhalil) for his development of [aioli and biowasm](https://github.com/biowasm).
---
+++ static/libs/subread.js
@@ -0,0 +1,233 @@
+document.getElementById("reference").addEventListener("change", loadRef, false);
+document.getElementById("fastq").addEventListener("change", loadFq, false);
+
+let align = new Aioli("subread-align/2.0.1");
+let buildindex = new Aioli("subread-buildindex/2.0.1");
+// Initialize
+// buildindex will be Aioli.workers[0]
+buildindex
+.init()
+.then(() => buildindex.exec(""))
+.then(d => console.log(d.stdout, "ERRRRR", d.stderr));
+
+// align will be Aioli.workers[1]
+align
+.init()
+.then(() => align.exec("-v"))
+.then(d => console.log(d.stdout, "ERRRRR", d.stderr));
+
+// make all bams
+async function makeAll(){
+    await transferIndex();
+    await delay(1000);
+    let suffix1 =  document.getElementById("suffix1").value; // R1 suffix
+    let filenames = document.getElementById("demoFq").innerHTML.split("\t");
+    let promises = [];
+    for (i = 0; i < filenames.length; i++) {
+        let ff = filenames[i];
+        if (ff.includes(suffix1)) {
+            // document.getElementById("bam").innerHTML = "Processing: " + ff;
+            let prefix = ff.replace(suffix1, "");
+            promises.push(makeBam(prefix));
+        }
+    }
+    await Promise.all(promises);
+    document.getElementById("bam").innerHTML = "All the files have been processed!";
+    document.getElementById("download-btn").style.visibility = "visible";
+}
+
+// make single bam
+async function makeBam (prefix, index="my_index") {
+    let wd = "/data/";
+    align.setwd(wd);
+    let suffix1 =  document.getElementById("suffix1").value; // R1 suffix
+    let suffix2 =  document.getElementById("suffix2").value; // R2 suffix
+    let R1 = prefix + suffix1;
+    let R2 = prefix + suffix2;
+    let out = prefix + ".bam";
+    let cmd = ["-i", index, "-r", R1, "-R", R2, "-o", out, "-t 1 -I 16 --sv --sortReadsByCoordinates"].join(' ');
+    console.log(cmd);
+    let std = await align.exec(cmd);
+    document.getElementById("bam").innerHTML = "... Making bam file " + out;
+    console.log(std.stderr);
+    console.log("Finished writing ", out);
+    document.getElementById("stderr").value += std.stderr + "\n";
+}
+
+function loadFq(event)
+{
+    // document.getElementById("demoFq").innerHTML = "";
+    // var filenames = [];
+    var files = event.target.files;
+    for (var i = 0, f; f = files[i]; i++) {
+        Aioli.mount(f, null, null, Aioli.workers[1]);// only to worker align
+        document.getElementById("demoFq").innerHTML += f.name + "\t";
+    }
+}
+
+async function loadRef(event)
+{
+    let files = event.target.files;
+    let f = files[0];
+    document.getElementById("demoRef").innerHTML = f.name;
+    await Aioli.mount(f, null, null, Aioli.workers[0]); // only to worker buildindex
+    await delay(500);
+    buildindex.ls("/data").then(console.log);
+    // index
+    buildindex.exec("-M 1000 -o /data/my_index /data/" + f.name)
+    .then(std => document.getElementById("stderr").value += std.stderr + "\n");
+    let ff = await buildindex.ls("/data");
+    if (ff.length < 8) {
+        document.getElementById("indexErr").innerHTML = "Indexing the reference FAILED. Please refresh the page to retry!";
+    }
+}
+
+// delay before
+const delay = ms => new Promise(res => setTimeout(res, ms));
+// transfer sam files to samtools /data
+async function transferIndex(){
+    let files = await buildindex.ls("/data"); // an array of files
+    // let promises = [];
+    for (var i = 0, f; f = files[i]; i++) {
+        if (f.includes("my_index")) {
+            // promises.push(Aioli.transfer("/data/" + f, "/data/" + f, Aioli.workers[0], Aioli.workers[1]));
+            Aioli.transfer("/data/" + f, "/data/" + f, Aioli.workers[0], Aioli.workers[1]);
+        }
+    }
+    // await Promise.all(promises);
+    await delay(1000);
+    console.log("Finished transfering files!");
+    return 0;
+}
+
+// download all the output files as a zip file
+// samtools.downloadBinary("/samtools/examples/out2.bam.bai").then(d => saveAs(d, "download.bam"));
+async function downloadBam(){
+    let files = await align.ls("/data"); // an array of files
+    let zip = new JSZip();
+    let promises = [];
+    for (let i = 0, f; f = files[i]; i++) {
+        if (f.includes(".bam")) {
+            console.log("Prepare downloading ", f);
+            let aa = await align.downloadBinary("/data/" + f).then(d => d.arrayBuffer()).then(d => zip.file(f, d));
+            promises.push(aa);
+        }
+    }
+    const d = await Promise.all(promises);
+    console.log("Finished preparing downloanding bams!");
+	zip.generateAsync({type:"blob"})
+		.then(function(content) {
+			saveAs(content, "indexed_bams.zip");
+		});
+}
+
+// download the indexed reference files
+async function downloadIndex(){
+    let files = await buildindex.ls("/data"); // an array of files
+    let zip = new JSZip();
+    let promises = [];
+    for (let i = 0, f; f = files[i]; i++) {
+        if (f.includes("my_index")) {
+            console.log("Prepare downloading ", f);
+            let aa = await buildindex.downloadBinary("/data/" + f).then(d => d.arrayBuffer()).then(d => zip.file(f, d));
+            promises.push(aa);
+        }
+    }
+    const d = await Promise.all(promises);
+    console.log("Finished preparing downloanding indexed references!");
+	zip.generateAsync({type:"blob"})
+		.then(function(content) {
+			saveAs(content, "indexed_reference_files.zip");
+		});
+}
+
+// merge all the indel.vcf files
+async function merge_indels(){
+    let files = await align.ls("/data"); // an array of files
+    let promises = [];
+    let indelSummary = "Sample,Gene,POS,REF,ALT,QUAL,TotalCoverage,indelCoverage,indelPercent,indelSize\n";
+    for (let i = 0, f; f = files[i]; i++) {
+        if (f.includes(".bam.indel.vcf")) {
+            let aa = await process_indel_vcf(f);
+            promises.push(aa);
+        }
+    }
+    const d = await Promise.all(promises);
+    indelSummary += promises.join("");
+    let blob = new Blob([indelSummary], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "Summary_of_indels_less_or_equal_16_bp.csv");
+}
+
+// process indel vcf content for only 1 file
+async function process_indel_vcf(f){//filename
+    let filename = f.replace(".bam.indel.vcf", "");
+    let vcf = await align.cat("/data/" + f);
+    let lines = vcf.split(/\r?\n/);
+    let summary = "";
+    for (let line of lines){
+        if (line && !line.includes("#")){
+            let ss = line.split(/\t/);
+            let DP = ss[7].replace("INDEL;DP=", "").split(";SR="); // DP and SR
+            let pct = String(parseInt(DP[1]) / parseInt(DP[0]) * 100); // percent of indels
+            let size = String(ss[4].length - ss[3].length);
+            summary += [filename, ss[0], ss[1], ss[3], ss[4], ss[5], DP[0], DP[1], pct, size].join(',') + "\n";
+        }
+    }
+    return summary;
+}
+
+// merge all the .bam.breakpoints.vcf files
+async function merge_sv(){ // structure variations and big indels > 15bp
+    let files = await align.ls("/data"); // an array of files
+    let promises = [];
+    let indelSummary = "Sample,Gene,Start,End,indelCoverage,indelSize\n";
+    for (let i = 0, f; f = files[i]; i++) {
+        if (f.includes(".bam.breakpoints.vcf")) {
+            let aa = await process_sv_vcf(f);
+            promises.push(aa);
+        }
+    }
+    const d = await Promise.all(promises);
+    indelSummary += promises.join("");
+    let blob = new Blob([indelSummary], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "Summary_of_indels_more_than_16_bp.csv");
+}
+
+// process indel vcf content for only 1 file
+async function process_sv_vcf(f){//filename
+    let filename = f.replace(".bam.breakpoints.vcf", "");
+    let vcf = await align.cat("/data/" + f);
+    let lines = vcf.split(/\r?\n/);
+    let summary = "";
+    let n = 0;
+    let start = "";
+    for (let line of lines){
+        if (line && !line.includes("#")){
+            n += 1;
+            let ss = line.split(/\t/);
+            if(n % 2 == 1){
+                summary += [filename, ss[0], ss[1]].join(',') + ",";
+                start = ss[1];
+            } else {
+                let SR = ss[7].split(";SR=")[1]; // SR
+                let size = String(parseInt(ss[1]) - parseInt(start)); // indel size
+                summary += [ss[1], SR, size].join(',') + "\n";
+            }
+        }
+    }
+    return summary;
+}
+
+// function to download running summary
+function download_stderr(){
+    runningSummary = document.getElementById("stderr").value;
+    let blob = new Blob([runningSummary], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "Software_running_summary.txt");
+}
+// download all files at once
+function downloadAll(){
+    downloadBam();
+    merge_indels();
+    merge_sv();
+    download_stderr();
+}
