3fda3e55a3b767b18f8b853baf189c17556f4cac
Junli Zhang <zhjl86@gmail.com>
2021-03-07 23:43:23 -0800

    add tool format fasta

---
+++ content/apps/2021-03-07-format-fasta.md
@@ -0,0 +1,28 @@
+---
+title: Format FASTA to fixed length
+author: Junli Zhang
+date: '2021-03-07'
+slug: format-fasta
+categories:
+  - Method
+tags:
+  - fasta
+---
+
+This tool format a fasta file to fix line length, which is required by some software, such as `subread`.
+
+<label for="fasta">Select a FASTA file</label>:
+<input type="file" id="fasta"/><br>
+<label for="line_width">Line width to format (bp)</label>
+<input id="line_width" value="60" size="4"><br>
+</form>
+<button id="download-btn" onclick="download()">Download formatted fasta file</button>
+<pre id="formatted" style="font-size:11px;"></pre>
+
+<script src="/libs/format_fasta.js"></script>
+<script src="/libs/FileSaver.min.js"></script>
+
+
+## Acknowledgement
+
+- The originial script is from: https://www.biostars.org/p/185103/
---
+++ static/libs/format_fasta.js
@@ -0,0 +1,46 @@
+function formatFasta(evt) {
+	var files = evt.target.files; // FileList object
+	if(files.length==0) return;
+	var pre = document.getElementById("formatted");
+	// while(pre.hasChildNodes()) pre.removeChild( pre.firstChild );
+    pre.innerText = "";
+	
+    var reader = new FileReader();
+    reader.onprogress=function(evt) {}
+    reader.onloadend=function(e) {
+        if(e.target.result==null) return;
+        var lines=e.target.result.split(/\r?\n/);
+        var dna="";
+        var title="";
+        var i=0;
+        var line;
+        let width = parseInt(document.getElementById("line_width").value);
+        for(;;){
+            if(i==lines.length || (line=lines[i])[0]=='>'){
+                if(dna.length != 0){
+                    pre.appendChild(document.createTextNode(title+"\n"));
+                    while(dna.length != 0){
+                        var n=Math.min(dna.length, width);
+                        pre.appendChild(document.createTextNode(dna.substring(0,n)+"\n"));
+                        dna= dna.substring(n);
+                    }
+                }
+                if(i===lines.length) break;
+                title=line;
+                dna="";
+            }
+            else dna+=line.trim();
+            ++i;
+        }
+    };
+    reader.readAsText(files[0]);
+}
+
+// download the formatted fasta file
+function download(){
+    fasta = document.getElementById("formatted").innerText;
+    let blob = new Blob([fasta], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "formatted_fasta.fa");
+}
+
+document.getElementById('fasta').addEventListener('change', formatFasta, false);
