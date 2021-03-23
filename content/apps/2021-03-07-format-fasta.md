adfc72f020e7d3d968a25958d28aac3b25fc6e6b
Junli Zhang <zhjl86@gmail.com>
2021-03-23 10:57:48 -0700

    add paste option

--- content/apps/2021-03-07-format-fasta.md
+++ content/apps/2021-03-07-format-fasta.md
@@ -15,7 +15,11 @@ This tool format a fasta file to fixed line length, which is required by some so
 <input type="file" id="fasta"/><br>
 <label for="line_width">Line length to format (bp)</label>
 <input id="line_width" value="60" size="4"><br>
-</form>
+<label for="paste">**OR Paste your sequences here in fasta format**</label><br>
+<textarea id="paste" name="paste" rows="10" cols="60" placeholder="Paste your sequences"></textarea><br>
+<button onclick="formatFastaPaste()">Format</button>
+<button onclick="clearseq()">Reset</button>
+
 <button id="download-btn" onclick="download()">Download formatted fasta file</button>
 <pre id="formatted" style="font-size:11px;"></pre>
 
--- static/libs/format_fasta.js
+++ static/libs/format_fasta.js
@@ -1,39 +1,21 @@
-function formatFasta(evt) {
+document.getElementById('fasta').addEventListener('change', formatFastaFile, false);
+
+async function formatFastaFile(evt) {
 	var files = evt.target.files; // FileList object
 	if(files.length==0) return;
 	var pre = document.getElementById("formatted");
 	// while(pre.hasChildNodes()) pre.removeChild( pre.firstChild );
-    pre.innerText = "";
-	
-    var reader = new FileReader();
-    reader.onprogress=function(evt) {}
-    reader.onloadend=function(e) {
-        if(e.target.result==null) return;
-        var lines=e.target.result.split(/\r?\n/);
-        var dna="";
-        var title="";
-        var i=0;
-        var line;
-        let width = parseInt(document.getElementById("line_width").value);
-        for(;;){
-            if(i==lines.length || (line=lines[i].trim())[0]=='>'){// trim to avoid space at the beginning
-                if(dna.length != 0){
-                    pre.appendChild(document.createTextNode(title+"\n"));
-                    while(dna.length != 0){
-                        var n=Math.min(dna.length, width);
-                        pre.appendChild(document.createTextNode(dna.substring(0,n)+"\n"));
-                        dna= dna.substring(n);
-                    }
-                }
-                if(i===lines.length) break;
-                title=line.replace(/> +/, '>');// in case something like "> seq1"
-                dna="";
-            }
-            else dna+=line.trim().replace(/ +/g,""); // in case space inside
-            ++i;
-        }
-    };
-    reader.readAsText(files[0]);
+    // pre.innerText = "";
+    let fileContent = await readTextFileAsync(files[0]);
+    let width = parseInt(document.getElementById("line_width").value);
+    pre.innerText = formatFasta(fileContent, width);
+}
+
+function formatFastaPaste() {
+    let fileContent = document.getElementById("paste").value;
+    let width = parseInt(document.getElementById("line_width").value);
+    var pre = document.getElementById("formatted");
+    pre.innerText = formatFasta(fileContent, width);
 }
 
 // download the formatted fasta file
@@ -43,4 +25,48 @@ function download(){
     saveAs(blob, "formatted_fasta.fa");
 }
 
-document.getElementById('fasta').addEventListener('change', formatFasta, false);
+
+// clear input sequences
+function clearseq() {
+    document.getElementById("paste").value = "";
+    document.getElementById("formatted").innerText = "";
+};
+
+// function to read a text file async, for loadRef
+function readTextFileAsync(file) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onload = () => {
+            resolve(new TextDecoder('utf-8').decode(reader.result, {stream: true}));
+        };
+        reader.onerror = reject;
+        reader.readAsArrayBuffer(file);
+    });
+}
+
+// function to format fasta to fixed length
+function formatFasta (fileContent, width = 60) {// 60 bp per line
+    let lines = fileContent.split(/\r?\n/);
+    let dna = "";   // all the bases of 1 seq (removed new lines)
+    let title = ""; // title of seq
+    let line = "";  // content of each line
+    let i = 0;      // line number
+    let newContent = "";
+    for (;;){
+        if(i==lines.length || (line=lines[i].trim())[0]=='>'){// trim to avoid space at the beginning
+            if(dna.length != 0){
+                newContent += title + "\n";
+                while(dna.length != 0){
+                    let n = Math.min(dna.length, width);
+                    newContent += dna.substring(0,n) + "\n";
+                    dna = dna.substring(n);
+                }
+            }
+            if(i===lines.length) break;
+            title = line.replace(/> +/, '>');// in case something like "> seq1"
+            dna = "";
+        } else dna += line.trim().replace(/ +/g, ""); // in case space inside
+        ++i;
+    }
+    return newContent;
+}
