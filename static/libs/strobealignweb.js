ed677052fd6f62544cc2a774a8af5d05adbdd92b
Junli Zhang <zhjl86@gmail.com>
2022-04-26 08:10:42 -0700

    format input fasta first for exactSNP

--- static/libs/strobealignweb.js
+++ static/libs/strobealignweb.js
@@ -41,6 +41,8 @@ async function downloadBam(){
     //         promises.push(aa);
     //     }
     // }
+    let bb = exactSNP.downloadBinary("/data/formatted_references.fa").then(d => d.arrayBuffer()).then(d => zip.file("formatted_references.fa", d));
+    promises.push(bb);
     let cc = merge_indels().then(d => d.arrayBuffer()).then(d => zip.file("Summary_of_SNPs_and_small_indels.txt", d));
     promises.push(cc);
     await Promise.all(promises);
@@ -141,18 +143,18 @@ async function loadRef(event)
 {
     let files = event.target.files;
     let f = files[0];
-    document.getElementById("demoRef").innerHTML = f.name;
-    await Aioli.mount(f, null, null, bwa) // return new file path
+    let infile = "formatted_references.fa";
+    document.getElementById("demoRef").innerHTML = infile; //f.name;
+    await Aioli.mount(f, "ref.fa", null, bwa) // return new file path
     .then(() => bwa.ls("/data"))
     .then(d => console.log(d));
     // index
-    // await delay(1000); // mount did not really await
-    // await bwa.exec("index /data/" + f.name)
-    // .then(d => console.log(d.stdout, "End of stdout\n", d.stderr, "End of stderr"));
-    // let ff = await bwa.ls("/data");
-    // if (ff.length < 8) {
-    //     document.getElementById("indexErr").innerHTML = "Indexing the reference FAILED. Please refresh the page to retry!";
-    // }
+    await delay(1000); // mount did not really await
+    let refContent = await bwa.cat("/data/ref.fa");
+    let newfile = {};
+    newfile.name = "/data/formatted_references.fa";
+    newfile.content = formatFasta(refContent);
+    bwa.write(newfile);
 }
 
 // run bwa mem on all fastq files
@@ -355,4 +357,31 @@ async function parseBed(content) {
     }
     depDict[tempGene] = tempDict;
     return depDict;
+}
+
+// function to format fasta to fixed length
+function formatFasta (fileContent, width = 60) {// 60 bp per line
+    let lines = fileContent.split(/\r?\n/);
+    let dna = "";   // all the bases of 1 seq (removed new lines)
+    let title = ""; // title of seq
+    let line = "";  // content of each line
+    let i = 0;      // line number
+    let newContent = "";
+    for (;;){
+        if(i==lines.length || (line=lines[i].trim())[0]=='>'){// trim to avoid space at the beginning
+            if(dna.length != 0){
+                newContent += title + "\n";
+                while(dna.length != 0){
+                    let n = Math.min(dna.length, width);
+                    newContent += dna.substring(0,n) + "\n";
+                    dna = dna.substring(n);
+                }
+            }
+            if(i===lines.length) break;
+            title = line.replace(/> +/, '>');// in case something like "> seq1"
+            dna = "";
+        } else dna += line.trim().replace(/ +/g, ""); // in case space inside
+        ++i;
+    }
+    return newContent;
 }
