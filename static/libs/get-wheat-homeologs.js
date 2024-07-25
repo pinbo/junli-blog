1ad2f9ba8ecbbdf57ff323e7346ebb196a69b215
Junli Zhang <zhjl86@gmail.com>
2024-07-25 12:00:46 -0700

    Use v3 db with 0.6 length cut

--- static/libs/get-wheat-homeologs.js
+++ static/libs/get-wheat-homeologs.js
@@ -36,7 +36,7 @@ async function getHomeolog(){
     let buf;
     if (database == "Kronos_cDNA_v1.0") {
         genePrefix = "TrturKRN";
-        dbURL = "https://dl.dropboxusercontent.com/scl/fi/f40srzyyp8mw5kn6zaqh6/Kronos_Homeolog_with_function2.db?rlkey=pyh3t76nroonxol1uueytekwx&st=mf42zky3";
+        dbURL = "https://jzseqbucket.s3.us-east-2.amazonaws.com/Kronos_Homeolog_with_function3.db"
         if ("Kronos" in buffers){
             buf = buffers.Kronos;
         } else {
@@ -46,7 +46,7 @@ async function getHomeolog(){
     }
     else if (database == "CS_cDNA_HC_v1.1") {
         genePrefix = "TraesCS";
-        dbURL = "https://dl.dropboxusercontent.com/scl/fi/03yjxb83ji3gu2aijg5o2/CS_Homeolog_with_function2.db?rlkey=9sao48v8bc2uqhekqdzarrrjg&st=uy3glz84";
+        dbURL = "https://jzseqbucket.s3.us-east-2.amazonaws.com/CS_Homeolog_with_function3.db";
         if ("CSv1HC" in buffers){
             buf = buffers.CSv1HC;
         } else {
@@ -74,19 +74,17 @@ async function getHomeolog(){
   d2.name As c2, \
   t.pctIdent As c3, \
   d3.name As c4, \
-  At_hit.pctIdent As c5, \
+  d2.At_ident As c5, \
   d3.description As c6, \
   d4.name As c7, \
-  Os_hit.pctIdent As c8, \
+  d2.Os_ident As c8, \
   d4.description As c9 \
 From wheat_hit t \
-Join wheatID d1 On ( d1.gene = t.gene ) \
-Join wheatID d2 On ( d2.gene = t.hit ) \
-Join At_hit      On ( At_hit.gene = t.hit ) \
-Join Os_hit      On ( Os_hit.gene = t.hit ) \
-Join AtOsID d3 On ( d3.gene = At_hit.hit ) \
-Join AtOsID d4 On ( d4.gene = Os_hit.hit ) \
-INNER JOIN hello ON hello.gene = d1.name \
+Join wheatID_and_hits d1 On ( d1.gene = t.gene ) \
+Join wheatID_and_hits d2 On ( d2.gene = t.hit ) \
+Left Join AtOsID d3 On ( d3.gene = d2.AtID ) \
+Left Join AtOsID d4 On ( d4.gene = d2.OsID ) \
+Join hello ON hello.gene = d1.name \
 ORDER BY d1.name;";
     // const stmt = db.prepare("SELECT * FROM Kronos_Homeolog WHERE gene = $gene");
     const stmt = db.prepare(sqlstr);
