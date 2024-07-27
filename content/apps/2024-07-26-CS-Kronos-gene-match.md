5da72c176dd38cae6ff0272a00567550bb19a55b
Junli Zhang <zhjl86@gmail.com>
2024-07-26 23:32:58 -0700

    new tool to convert gene IDs for wheat

---
+++ content/apps/2024-07-26-CS-Kronos-gene-match.md
@@ -0,0 +1,63 @@
+---
+title: 'Gene ID conversion between Kronos and CS'
+author: Junli Zhang
+date: '2024-07-26'
+slug: CS-Kronos-gene-match
+categories:
+  - tools
+tags:
+  - wheat
+  - Kronos
+  - CS
+  - sqlite3
+---
+
+Match the genes in CS RefSeq v1.1 annotation HC with Kronos v1.0 annotation (best hit with >97% identity and alignment length > 100 bp).
+
+I did not do more filtering than that, so some genes may have multiple hits or not on the same chromosome. 
+
+<p style="color:red";>Select with your own eyes based on chromosome matches and %identity.</p>
+
+Please paste gene IDs (e.g. TraesCS5A02G391700) below.
+*Each line is a gene*.
+
+**Gene ID conversion**:
+<select id="box1">
+  <option value="K2C">Kronos to CS</option>
+  <option value="C2K">CS to Kronos</option>
+</select>
+
+<textarea rows="10" cols="75" id="input"></textarea>
+<br />
+
+<button id="run">Submit</button>
+<button id="clearseq">Clear</button>
+<button id="example">Example</button>
+
+**Output below**
+
+<!-- <textarea rows="10" cols="75" id="output" ></textarea> -->
+<!-- <br /> -->
+<p id="alert" style="color:blue";></p>
+<button id="copytable">Copy to clipboard</button>
+<a download="geneID-conversion-Kronos-CS.csv" href="#" onclick="return ExcellentExport.csv(this, 'datatable');" style="color:Tomato;">or Export to CSV</a>
+<!-- The button used to copy the text -->
+
+<table id="datatable" style="font-size: 11px;" align="left">
+<thead>
+    <tr>
+        <th>Kronos GeneID</th>
+        <th>CS GeneID</th>
+        <th>%identity</th>
+        <th>alignment length</th>
+        <th>Kronos CDS length</th>
+        <th>CS CDS length</th>
+    </tr>
+</thead>
+    <tbody id="tbody"></tbody>
+</table>
+
+<script src="/tools/sqljs/v1.10.3/sql-wasm.js"></script>
+<script type="module" src="/libs/geneID-conversion-Kronos-CS.js"></script>
+<script src="/libs/excellentexport.min.js"></script>
+<script src="/libs/pako_inflate.min.js"></script>
---
+++ static/libs/geneID-conversion-Kronos-CS.js
@@ -0,0 +1,115 @@
+const initSqlJs = window.initSqlJs;
+// const SQL = await initSqlJs({
+//     // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
+//     // You can omit locateFile completely when running in node
+//     locateFile: file => `/tools/sqljs/v1.10.3/sql-wasm.wasm`
+//   });
+
+const sqlPromise = initSqlJs({
+locateFile: file => `/tools/sqljs/v1.10.3/sql-wasm.wasm`
+});
+const dataPromise = fetch("https://jzseqbucket.s3.us-east-2.amazonaws.com/cDNA_Kronos_v1_vs_CS_HC_v1.db.gz").then(res => res.arrayBuffer()).then(raw => pako.inflate(raw));
+const [SQL, buf] = await Promise.all([sqlPromise, dataPromise])
+const db = new SQL.Database(new Uint8Array(buf));
+
+
+// trim white spaces
+function mytrim(x) {
+    return x.replace(/^\s+|\s+$/g,'');
+}
+
+
+// get homeolog
+async function getHomeolog(){
+    // document.getElementById("output").value = "";
+    document.getElementById("alert").innerText = "Working on it... Be patient...";
+    let inputString = document.getElementById("input").value.replace(/^\s+|\s+$/g,'');
+    let lines = inputString.split('\n');
+    let sel = document.getElementById("box1"); // box1 is selection of todo
+    let todo = sel.options[sel.selectedIndex].value;
+    // let genePrefix = "TrturKRN";
+    let wheat = "Kronos";
+    if (todo == "C2K") {
+        // genePrefix = "TraesCS";
+        wheat = "CS";
+    }
+    // prepare input cmd
+    let whereStr = "";
+    for(let i = 0; i < lines.length; i++){
+		let ll = mytrim(lines[i]);
+        if (ll){
+            let gene = ll; //ll.replace(genePrefix, "");
+            console.log(gene);
+            whereStr += "'" + gene + "',";
+	    }
+    }
+    whereStr = whereStr.replace(/,$/, '');
+    console.log(whereStr);
+    let sqlstr = "SELECT * FROM hits WHERE hits." + wheat + " IN (" + whereStr + ") ORDER BY " + wheat + ";";
+    console.log(sqlstr);
+    const stmt = db.prepare(sqlstr);
+    // stmt.bind({$gene:whereStr});
+    // const row = stmt.getAsObject({});
+    // console.log('Here is a row: ' + JSON.stringify(row));
+    document.getElementById('tbody').innerHTML = "";
+    while(stmt.step()) { //
+        const row = stmt.getAsObject();
+        console.log('Here is a row: ' + JSON.stringify(row));
+        let text = "";
+        let k = '<tr>';
+        for (let x in row) {
+            text += row[x] + "\t";
+            k+= '<td>' + row[x] + '</td>';
+        }
+        k+= '</tr>';
+        // document.getElementById("output").value += text + "\n";
+        document.getElementById('tbody').innerHTML += k;
+    }
+    stmt.free(); // free the memory used by the statement
+    document.getElementById("alert").innerText = "Done!";
+
+}
+
+
+// give examples
+function giveExamples(){
+    let sel = document.getElementById("box1"); // box1 is selection of todo
+    let todo = sel.options[sel.selectedIndex].value;
+    if (todo == "K2C") document.getElementById("input").value = "TrturKRN6B01G025800\nTrturKRN7A01G081900";
+    else if (todo == "C2K") document.getElementById("input").value = "TraesCS5A02G391700\nTraesCS5B02G396600";
+}
+
+function copy2clipboard() {
+    // Get the text field
+    let tableElement = document.getElementById("datatable");
+  
+    // Convert the table element to an HTML string
+    const tableHTMLString = tableElement.outerHTML;
+
+    // Convert the table element to plain text string
+    const plainTextString = tableElement.innerText;
+
+    navigator.clipboard.write([
+        new ClipboardItem({
+            'text/html': new Blob([tableHTMLString], {
+                type: 'text/html',
+            }),
+            'text/plain': new Blob([plainTextString], {
+                type: 'text/plain',
+            }),
+        }),
+    ]);
+}
+
+// clear input box
+function clearseq() {
+	document.getElementById("input").value = "";
+	// document.getElementById("output").value = "";
+    document.getElementById('tbody').innerHTML = "";
+    document.getElementById("alert").innerText = "";
+};
+
+document.getElementById("run").addEventListener("click", getHomeolog);
+document.getElementById("clearseq").addEventListener("click", clearseq);
+document.getElementById("example").addEventListener("click", giveExamples);
+document.getElementById("copytable").addEventListener("click", copy2clipboard);
